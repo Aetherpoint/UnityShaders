95691abc275986921f5cc78a8b6826664bbbefe1
Andrew Johnson <aetherpoint@gmail.com>
2018-07-06 01:56:32 -0400

    Adds a couple more common scripts

---
+++ Utility Scripts/FadeOut.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeOut : MonoBehaviour {
+
+    public float killTime = 2f; // Seconds to read the text
+    public Color currentColor;
+    public Color colorFaded;
+
+    private TextMesh text;
+    private bool fading = false;
+
+    float t = 0;
+    private float duration = 11f;
+
+    void Awake()
+    {
+        text = GetComponent<TextMesh>();
+    }
+
+    void Update()
+    {
+        if (t < 1)
+        {
+            text.color = Color.Lerp(currentColor, colorFaded, t);
+            t += Time.deltaTime / duration;
+        }
+    }
+}
+
---
+++ Utility Scripts/GetFromWeb.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class GetFromWeb : MonoBehaviour {
+
+    public string webText;
+    public string jsonSource = "http://aetherpoint.com/artext/items.json";
+    private bool searching = true;
+    private bool searchReady = true;
+    public int searchingTime = 10;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(SetTimer());
+	}
+
+    // Poll for data updates
+    IEnumerator SetTimer() {
+        while (searching == true) {
+
+            Debug.Log("Attempting search " + searchReady);
+
+            if (searchReady) {
+
+                UnityWebRequest www = UnityWebRequest.Get(jsonSource);
+                yield return www.SendWebRequest();
+
+                if (www.isNetworkError || www.isHttpError) {
+                    Debug.Log(www.error);
+                }
+                else {
+                    // Show results as text
+                    Debug.Log(www.downloadHandler.text);
+
+                    // Or retrieve results as binary data
+                    byte[] results = www.downloadHandler.data;
+
+                    webText = www.downloadHandler.text;
+                }
+
+                yield return new WaitForSeconds(searchingTime);
+            }
+        }
+    }
+
+
+	private void Update()
+	{
+        gameObject.GetComponent<TextMesh>().text = webText;
+	}
+}
