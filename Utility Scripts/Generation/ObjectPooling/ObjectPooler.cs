c0f4ea12975cbda71387402617094ab3bd610a93
Andrew Johnson <aetherpoint@gmail.com>
2018-09-07 23:37:30 -0400

    Adds object pooling script

---
+++ Utility Scripts/Generation/ObjectPooling/ObjectPooler.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour {
+    
+    // set of game objecta to spawn
+    // based on available letters
+    public List<GameObject> alphaBlock;
+
+    [System.Serializable]
+    public class Pool {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+    // Singleton
+    public static ObjectPooler Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+
+    void Start () {
+        
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach (Pool pool in pools) {
+            
+            Queue<GameObject> ObjectPool = new Queue<GameObject>();
+
+            for (int i = 0; i < pool.size; i++) {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                ObjectPool.Enqueue(obj);
+            }
+
+            poolDictionary.Add(pool.tag, ObjectPool);
+        }
+    }
+
+    public GameObject SpawnFromPool (string tag, Vector3 position, Quaternion rotation) {
+
+        if (!poolDictionary.ContainsKey(tag)) {
+            Debug.LogWarning("Pool with tag " + tag + " doesn’t exist.");
+            return null;
+        }
+
+        GameObject objectToSpawn = poolDictionary[tag].Dequeue();
+
+        objectToSpawn.SetActive(true);
+        objectToSpawn.transform.position = position;
+        objectToSpawn.transform.rotation = rotation;
+
+        poolDictionary[tag].Enqueue(objectToSpawn);
+
+        return objectToSpawn;
+    }
+
+
+}
---
+++ Utility Scripts/Generation/ObjectPooling/Spawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    private int timer = 0;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        
+        if (timer > 30) {
+            timer = 0;
+            ObjectPooler.Instance.SpawnFromPool("alphablock", transform.position, Quaternion.identity); 
+        }
+        else {
+            timer++;
+        }
+
+    }
+}
